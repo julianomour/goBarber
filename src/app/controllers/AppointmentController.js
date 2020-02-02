76ab2dc32dc78e15c688cff20a45b8a5a2812713
Juliano Moura <julianomoura05@gmail.com>
2020-02-02 01:11:30 -0300

    creating notification and save into mongodb

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -59,9 +61,14 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    if (provider_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot create a appointment for yourself' });
+    }
+
     // check for past dates
     const hourStart = startOfHour(parseISO(date));
-
     if (isBefore(hourStart, new Date())) {
       res.status(400).json({ error: 'Past date is not permitted' });
     }
@@ -81,6 +88,20 @@ class AppointmentController {
       provider_id,
       date: hourStart,
     });
+
+    // Notify appointment provider
+
+    const user = await User.findByPk(req.userId);
+
+    const formatedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+      locale: pt,
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formatedDate}`,
+      user: provider_id,
+    });
+
     return res.json(appointment);
   }
 }
